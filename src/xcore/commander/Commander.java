402f3f90e9b956a90a059a8be9d975827dfdc82e
Arthur <apploidyakutsk@gmail.com>
2019-02-18 19:50:00 +0300

    Command import

--- src/xcore/commander/Commander.java
+++ src/xcore/commander/Commander.java
@@ -1,5 +1,7 @@
 package xcore.commander;
 
+import xcore.commander.commands.Command;
+
 /**
  * @author Arthur Kupriyanov
  * @version 1.0
@@ -7,6 +9,10 @@ package xcore.commander;
 public class Commander {
     public static String getResponse(String request){
         return CommandDeterminant.getCommand(CommandManager.getCommands(), request).exec(request);
+    }
+
+    public static void addCommand(Command command){
+        CommandManager.addCommand(command);
     }
 
 }
