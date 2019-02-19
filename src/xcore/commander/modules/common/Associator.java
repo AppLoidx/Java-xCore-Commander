afb28d3e2bf0c2690ae08d6db1bb2992cddb3f53
Arthur <apploidyakutsk@gmail.com>
2019-02-19 23:35:06 +0300

    initial commit

---
+++ src/xcore/commander/modules/common/Associator.java
@@ -0,0 +1,69 @@
+package xcore.commander.modules.common;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Objects;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class Associator <K,V>{
+    public HashMap<HashSet<K>,V> associations = new HashMap<>();
+    private HashSet<V> values = new HashSet<>();
+
+    public void putAssociation(@NotNull HashSet<K> hashSetKey, V value){
+        if (values.contains(value)){
+            HashSet<K> oldKey = null;
+            for(HashSet<K> k: associations.keySet() ){
+                if (associations.get(k).equals(value)){
+                    oldKey = k;
+                    break;
+                }
+            }
+            HashSet<K> newKey = oldKey;
+            Objects.requireNonNull(newKey).addAll(hashSetKey);
+            removeAsscociation(oldKey);
+            this.putAssociation(newKey, value);
+        }
+        associations.put(hashSetKey, value);
+        values.add(value);
+    }
+
+    private void removeAsscociation(HashSet<K> key){
+        values.remove(associations.get(key));
+        associations.remove(key);
+    }
+
+    public V getAssociation(K association) throws AssociationNotFoundException {
+        for(HashSet<K> k: associations.keySet() ){
+            Iterator<K> innerSetValue = k.iterator();
+            while(innerSetValue.hasNext()) {
+                if (innerSetValue.next().equals(association)) {
+                    return this.associations.get(k);
+                }
+            }
+        }
+
+        throw new AssociationNotFoundException("Ассоциация не найдена!");
+    }
+
+    public V tryGetAssociation(V association){
+        for(HashSet<K> k: associations.keySet() ){
+            if (k.equals(association)){
+                return this.associations.get(k);
+            }
+        }
+
+        return association;
+    }
+
+
+}
+class AssociationNotFoundException extends Exception{
+    AssociationNotFoundException(String msg){
+        super(msg, new Exception(), true, false);
+    }
+}
